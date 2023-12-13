e7f9ed6987dac5a0fcb30977ad9663dfdb0a841b
Andrew220708 <andrew.asmogilov@gmail.com>
2023-12-13 16:11:32 +0100

    update tusk-3
    commit

--- js/task-3.js
+++ js/task-3.js
@@ -13,7 +13,7 @@ class StringBuilder {
         this.#value = str + this.#value;
     }
     padBoth(str) {
-        this.#value = `${str}` + `${this.#value}` + `${str}`;
+        this.#value = str + this.#value + str;
     }
 }
 
